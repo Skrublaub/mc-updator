7f9a5743b8b41571da371515cdfbbdbd7166b4a2
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-06-28 22:40:54 -0500

    remove useless functions, add make_release

--- backend/github/parse_tools.py
+++ backend/github/parse_tools.py
@@ -3,7 +3,8 @@ import json
 
 from typing import Any
 
-from backend.constants import MC_RELEASES_URL_BASE, RELEASES_PER_PAGE, NAME_MATCH
+from backend.constants import MC_RELEASES_URL_BASE, RELEASES_PER_PAGE
+from backend.items import ReleaseItem
 
 
 def format_api_link(page_number: int = 1, amt_per_page: int = RELEASES_PER_PAGE) -> str:
@@ -11,8 +12,8 @@ def format_api_link(page_number: int = 1, amt_per_page: int = RELEASES_PER_PAGE)
     Formats a github api call link.
 
     Args:
-        page_number: What page number to get
-        amt_per_page: How many per page to return
+        page_number (int): What page number to get
+        amt_per_page (int): How many per page to return
 
     Returns:
         str: A formatted api git call
@@ -20,45 +21,14 @@ def format_api_link(page_number: int = 1, amt_per_page: int = RELEASES_PER_PAGE)
     return f'{MC_RELEASES_URL_BASE}?per_page={amt_per_page}&page={page_number}'
 
 
-def get_versions(amt_pages: int) -> list[tuple[str, str, list[Any]]]:
-    """
-    Gets the versions and id available to download.
-    The args will be used later
-
-    Args:
-        amt_pages (int): The amount of pages to look through
-        page_number (int): What page to start counting on
-
-    Returns:
-        list[tuple[str, str, list[Any]]]: Info about releases
-            Index 0: Release version name
-            Index 1: Release id
-            Index 2: Assets
-    """
-    versions_list: list[tuple[str, str, list[Any]]] = []
-
-    # add support for more than dev and latest release later
-    # for page_number in range(amt_pages):
-    page_number: int = 0
-    r = requests.get(format_api_link(page_number=(page_number + 1)))
-    r.raise_for_status()
-
-    r_json = json.loads(r.text)
-
-    for release in r_json:
-        versions_list.append((release['name'], release['id'], release['assets']))
-
-    return versions_list
-
-
-def get_amt_pages() -> int:
+def get_amt_pages(session: requests.Session) -> int:
     """
     Gets the amount of pages by parsing the headers of a given request.
 
     Returns:
         int: Amount of total pages
     """
-    r = requests.get(format_api_link())
+    r = session.get(format_api_link())
     r.raise_for_status()
 
     # r.headers is a dictionary
@@ -66,9 +36,22 @@ def get_amt_pages() -> int:
     return int(r.headers['link'].split()[2][-3])
 
 
-def find_zip(assets: list[Any]) -> str:
+def make_release(page_json: Any, index: int = 1) -> ReleaseItem:
+    return ReleaseItem(page_json[index]["name"], page_json[index]["id"], page_json[index]["assets"])
+
+'''def find_zip(assets: list[Any], choices: list[str]) -> str:
+    """
+    Looks through the chi
+    
+    Args:
+        assets: 
+        choices: 
+
+    Returns:
+
+    """
     for asset in assets:
         if asset['name'] == NAME_MATCH:
             return asset['browser_download_url']
 
-    raise IndexError(f"No {NAME_MATCH} found in the assets")
+    raise IndexError(f"No {NAME_MATCH} found in the assets")'''
