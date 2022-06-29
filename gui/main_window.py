233ec57e6744ed39edc1ebbc5a29c5101975f87c
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-06-28 22:40:10 -0500

    change imports, still not finished

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,20 +1,26 @@
-from PyQt6.QtWidgets import  QWidget
+import requests
+
+from backend.github.download_tools import start_requests_session
+from backend.github.parse_tools import get_amt_pages, format_api_link
+from PyQt6.QtWidgets import QWidget
 from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import
 from PyQt6.QtWidgets import QCheckBox, QBoxLayout
 
 from typing import Any
 
-from backend.github.parse_tools import get_versions
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
         icon = QIcon("gui/icons/mc.png")
-
         self.setWindowIcon(icon)
 
-        assets: list[Any] = get_versions(1)[2]
+        s: requests.Session = start_requests_session()
+
+        # amt_pages: int = get_amt_pages(s)  # could be used later
+
+        page_json: list[Any] = s.get(format_api_link()).json()
 
     def get_check_boxes(self, assets: list[Any]) -> QCheckBox:
+        pass
